4f3e803b3e23a586f094349aea41978c9ec3cce1
connorlindley <lindleyconnor12@gmail.com>
2026-03-20 18:27:09 +1000

    Styling and more Layout updates - adding a card background

--- App.js
+++ App.js
@@ -3,34 +3,48 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>The To-Do List</Text>
+      <Text style={styles.title}>THE TO-DO LIST</Text>
 
-      <Text style={styles.row}>Wash the Dishes.</Text>
-      <Text style={styles.row}>Do homework.</Text>
-      <Text style={styles.row}>Finish Assignment Work.</Text>
-      <Text style={styles.row}>Go for a walk with the dog.</Text>
-      <Text style={styles.row}>Complete the quiz.</Text>
+      <View style={styles.card}>
+        <Text style={styles.row}>Wash the Dishes</Text>
+        <Text style={styles.row}>Do homework</Text>
+        <Text style={styles.row}>Finish Assignment Work</Text>
+        <Text style={styles.row}>Walk the dog</Text>
+        <Text style={styles.row}>Complete the quiz</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  title: {
-    marginTop: 50,
-    fontSize: 20,
-    marginBottom: 10,
-    textAlign: "center",
-  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "flex-start",
+    backgroundColor: "#f2f4f7",
+    paddingTop: 60,
+    paddingHorizontal: 20,
   },
-  row: {
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#333",
+    borderBottomColor: "#333",
+    borderBottomWidth: 1,
+  },
+
+  card: {
+    backgroundColor: "#ffffff",
+    borderRadius: 12,
     padding: 10,
-    backgroundColor: "#b8b8b8",
-    margin: 10,
-    marginLeft: 20,
-    marginRight: 20,
+  },
+
+  row: {
+    fontSize: 16,
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    color: "#444",
   },
 });
